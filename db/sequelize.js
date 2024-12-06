d358eff328448d39f56bb69be940880ec1374960
leoschmitt <leoeyeschmittk@gmail.com>
2024-12-06 19:48:26 -0300

    feat: adicionando conexão db

--- db/sequelize.js
+++ db/sequelize.js
@@ -12,19 +12,4 @@ const sequelize = new Sequelize ('postgres', 'postgres', 'postgres123', {
     },
 });
 
-
-(async () => {
-    try{
-        await sequelize.authenticate();
-        await sequelize.sync();
-
-        console.log(`Conexão com o banco de dados foi bem-sucedida!`)
-    }catch(error){
-        console.log(`Não foi possível concectar ao banco de dados:`, error)
-    }
-
-})()
-
-
-
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const productsRoutes = require('./routes/productRoutes');
 
-const app = express();
-const PORT = 3000;
+// db
+const sequelize = require('./db/sequelize.js'); 
 
-// Middleware
-app.use(bodyParser.json());
+// cors
+const cors = require("cors");
 
-// Rotas
-app.use('/products', productsRoutes);
+(async () => {
+  try {
+    await sequelize.authenticate(); // conexão db 
+    console.log('\n------- Conexão com o banco de dados estabelecida com sucesso! -------\n');
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+    await sequelize.sync(); 
+    console.log('\n------- Modelos sincronizados com o banco de dados! -------\n');
+
+    // app init
+      const app = express();
+      const PORT = 3000;
+
+    // Middleware
+      app.use(bodyParser.json());
+      app.use(cors()); 
+
+    // Rotas
+    app.use('/products', productsRoutes);
+
+    app.get('/', (req, res) => {
+      res.send('Isso é um teste');
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados ou criar usuário: ', error);
+  }
+})();
+
+
+
+
