b03a3a9242f5d3865e254c6a06168182504c56ed
leoschmitt <leoeyeschmittk@gmail.com>
2024-12-06 19:57:32 -0300

    feat - all models

---
+++ models/Product.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize.js');
+const Category = require('./Category.js'); 
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  categoryId: {
+    type: DataTypes.UUID,
+    references: {
+      model: Category,
+      key: 'id',
+    },
+  },
+}, {
+  tableName: 'products',
+  timestamps: true,
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'categoryId',
+  as: 'category',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'categoryId',
+  as: 'products',
+});
+
+module.exports = Product;
