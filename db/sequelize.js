586a71a371d97518f95947c024e6d03f56322248
MARCOS JÚNIOR LEMES FERREIRA <marcos_j_ferreira@estudante.sesisenai.org.br>
2024-12-06 14:37:15 -0300

    teste

---
+++ db/sequelize.js
@@ -0,0 +1,22 @@
+import sequelize from 'sequelize'
+
+const Sequeliza = new sequelize ('intelbras', 'postgres', 'postgres', {
+    host:'localhost',
+    dialect: 'postgres',
+    logging: false
+})
+
+
+async function log() {
+    try{
+        await sequelize.autheticate();
+
+        console.log(`Conexão com o banco de dados foi bem-sucedida!`)
+    }catch(error){
+        console.log(`Não foi possível concectar ao banco de dados:`, error)
+    }
+
+}
+
+
+
