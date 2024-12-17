11802dd3e2f84319cb29e87281d41f43d8dc6fe1
leoschmitt <leoeyeschmittk@gmail.com>
2024-12-17 20:09:51 -0300

    feat: adding render url to crs

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const cors = require("cors");
     
       const allowedOrigins = [
         'http://127.0.0.1:5500',
+        'https://sa-intelbras-api.onrender.com',
         'https://main.d63kzttoveint.amplifyapp.com',
       ];
       
